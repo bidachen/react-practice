037b100cd28a081307d0205ef7d3cb3a21e34dbc
bidachen <39844391+bidachen@users.noreply.github.com>
2018-12-28 17:19:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -149,3 +149,107 @@ class App extends React.Component{
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+        
+        
+        
+        
+        class GetInput extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: '' }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(event) {
+    this.setState({ value: event.target.value })
+  }
+  render() {
+    return <input name={this.props.string} type="text" value={this.state.value} onChange={this.handleChange} />
+
+  }
+}
+
+class GetSelection extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: 'Science Lab' }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(event) {
+    this.setState({ value: event.target.value })
+  }
+  render() {
+    return <select name="selection" value={this.state.value} onChange={this.handleChange}>
+      <option>Science Lab</option>
+      <option>Swimming</option>
+      <option>Cooking</option>
+      <option>Painting</option>
+    </select>
+  }
+}
+
+class CheckBox extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { checked: false }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(event) {
+    this.setState({ checked: event.target.checked })
+  }
+  render() {
+    return <input name={this.props.name} type="checkbox" checked={this.state.checked} onChange={this.handleChange} />
+  }
+}
+
+function ResultTable(props) {
+  return <tr>
+    <th>{props.first}</th>
+    <th>{props.last}</th>
+    <th>{props.activity}</th>
+    <th>{props.restrictions}</th>
+  </tr>
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { list: [{ id: 0 }] }
+    this.addCourse = this.addCourse.bind(this)
+    this.delCourse = this.delCourse.bind(this)
+  }
+  addCourse(event) {
+    alert(1)
+  }
+  delCourse(event) {
+
+  }
+  render() {
+    let resultTable = this.state.list.map((item, index) => {
+      return <ResultTable id={item.id} first={item.first} last={item.last} activity={item.activity} restrictions={item.restrictions} />
+    });
+    return <form onSubmit={this.addCourse} id="newform">
+      <label>First name: <br />
+        <GetInput string={"First"} /><br />
+      </label>
+      <label>
+        Last name: <br></br>
+        <GetInput string={"Last"} /><br />
+      </label>
+      <GetSelection /><br />
+      <label>
+        <CheckBox name={'a'} />
+        a) Dietary Restrictions<br/>
+      </label>
+      <label>
+        <CheckBox name={'b'} />
+        b) Physical Disabilities<br/>
+      </label>
+      <label>
+        <CheckBox name={'c'} />
+        c) Medical Needs<br/>
+      </label>
+      <input type='submit' value='submit' />
+    </form>
+  }
+}
+
